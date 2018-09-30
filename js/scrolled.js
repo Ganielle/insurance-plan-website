8db32ede19b3ae499e328faa2c235d14150fbc4c
Ganielle <ganielledotexev2@gmail.com>
2018-10-01 05:46:56 +0800

    scrolled.js
    this is the js file wherein it uses scrollreveal.js

---
+++ js/scrolled.js
@@ -0,0 +1,232 @@
+$(document).ready(function(){
+
+
+	window.sr = ScrollReveal();
+
+	sr.reveal('#jumbopic',{
+		duration: 2000,
+		origin: 'left',
+		distance: '50px'
+	});
+
+	sr.reveal('#jumboquote',{
+		duration: 2000,
+		origin: 'left',
+		distance: '50px',
+		delay: 800
+	});
+
+	sr.reveal('#insurance',{
+		duration: 2000,
+		origin: 'left',
+		distance: '50px',
+		delay: 1400
+	});
+
+	sr.reveal('#jumbobutton',{
+		duration: 2000,
+		origin: 'left',
+		distance: '50px',
+		delay: 1600
+	});
+
+	sr.reveal('#family',{
+		duration: 2000,
+		origin: 'right',
+		distance: '10px'
+	});
+
+	sr.reveal('#insured',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 100,
+		reset: true
+	});
+
+	sr.reveal('#nulife',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#surveylogo',{
+		duration: 2000,
+		origin: 'left',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#loginlogo',{
+		duration: 2000,
+		origin: 'right',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#resultlogo',{
+		duration: 2000,
+		origin: 'left',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#verticalline',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#leftlogo',{
+		duration: 2000,
+		origin: 'right',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#rightlogo',{
+		duration: 2000,
+		origin: 'left',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#finalstep',{
+		duration: 2000,
+		origin: 'left',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#stepone',{
+		duration: 2000,
+		origin: 'left',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#steptwo',{
+		duration: 2000,
+		origin: 'right',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#lifeinsurance',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+	sr.reveal('#vehicleinsurance',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+	sr.reveal('#healthinsurance',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+	sr.reveal('#homeinsurance',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+	sr.reveal('#travelinsurance',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+	sr.reveal('#petinsurance',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#petinsurance',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+	sr.reveal('#steps',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#filipinos',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#plan',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#life',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#number',{
+		duration: 2000,
+		origin: 'bottom',
+		distance: '50px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#facebook',{
+		duration: 2000,
+		origin: 'left',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+
+	sr.reveal('#messenger',{
+		duration: 2000,
+		origin: 'right',
+		distance: '10px',
+		delay: 200,
+		reset: true
+	});
+});
